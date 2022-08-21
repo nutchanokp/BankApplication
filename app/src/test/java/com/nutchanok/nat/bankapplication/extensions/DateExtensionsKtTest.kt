b6af8f24ddac6406d997c309353c55a3035c2c95
Nutchanok <Nutchanok@feyverly.com>
2022-08-21 17:02:02 +0700

    edit

--- app/src/test/java/com/nutchanok/nat/bankapplication/extensions/DateExtensionsKtTest.kt
+++ app/src/test/java/com/nutchanok/nat/bankapplication/extensions/DateExtensionsKtTest.kt
@@ -1,11 +1,22 @@
 package com.nutchanok.nat.bankapplication.extensions
 
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import org.junit.Test
 
 class DateExtensionsKtTest {
 
     @Test
-    fun toDateFormat() {
+    fun toDateFormat_is_equal() {
+
+        val tasks = "2022-08-10 22:20:22"
+        val answer = "10 Aug 2022 - 22:20"
+
+        val result = (tasks).toDateFormat(DateFormat.FROM_SERVICE_TIME, DateFormat.SHOW)
+
+        MatcherAssert.assertThat(result, CoreMatchers.`is`(answer))
+
+
     }
 
 }
--- app/src/test/java/com/nutchanok/nat/bankapplication/extensions/OtherExtensionsKtTest.kt
+++ app/src/test/java/com/nutchanok/nat/bankapplication/extensions/OtherExtensionsKtTest.kt
@@ -1,12 +1,32 @@
 package com.nutchanok.nat.bankapplication.extensions
 
-import org.junit.Assert.*
-
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import org.junit.Test
 
 class OtherExtensionsKtTest {
 
     @Test
-    fun toCurrency() {
+    fun toCurrency_is_equal() {
+
+        val tasks = 123.212
+        val answer = "123.21"
+
+        val result = tasks.toCurrency()
+
+        MatcherAssert.assertThat(result, CoreMatchers.`is`(answer))
+
     }
+    @Test
+    fun toCurrency_is_not_equal() {
+
+        val tasks = 123.212
+        val answer = "123.210"
+
+        val result = tasks.toCurrency()
+
+        MatcherAssert.assertThat(result, CoreMatchers.not(answer))
+
+    }
+
 }
